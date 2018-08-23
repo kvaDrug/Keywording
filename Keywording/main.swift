57661c5190077be11372ec15486acabc18b9539d
Vladimir Kelin <vK@macbook-pro.local>
2018-08-23 21:48:45 +0300

    Initial Commit

---
+++ Keywording/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Keywording
+//
+//  Created by Vladimir Kelin on 8/23/18.
+//  Copyright © 2018 Vladimir Kelin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
